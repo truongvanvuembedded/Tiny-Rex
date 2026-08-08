8f58958aa02a56691b13dc0ce15904de379a532f
truongvanvuembedded <truongvanvuvta1@gmail.com>
2026-08-08 13:39:32 +0000

    feat(screen): Add draw ranking screen

--- application/sources/app/screens/scr_ranking.cpp
+++ application/sources/app/screens/scr_ranking.cpp
@@ -1,5 +1,7 @@
 #include "scr_ranking.h"
 
+#define RANKING_MAX 5
+
 static void view_scr_ranking();
 
 view_dynamic_t dyn_view_ranking = {
@@ -17,8 +19,25 @@ view_screen_t scr_ranking = {
 	.focus_item = 0,
 };
 
-void view_scr_ranking() {
+typedef struct
+{
+    const char* name;
+    uint16_t score;
+} ranking_t;
 
+static ranking_t g_ranking[RANKING_MAX] =
+{
+    {"VIP1", 0},
+    {"VIP2", 0},
+    {"VIP3", 0},
+    {"VIP4", 0},
+    {"VIP5", 0},
+};
+
+static void drawRanking(void);
+
+void view_scr_ranking() {
+	drawRanking();
 }
 
 void scr_ranking_handle(ak_msg_t *msg) {
@@ -31,9 +50,35 @@ void scr_ranking_handle(ak_msg_t *msg) {
 	case AC_DISPLAY_BUTON_DOWN_PRESSED:
 	{
 		SCREEN_TRAN(scr_menu_handle, &scr_menu);
+		BUZZER_PlaySound(BUZZER_SOUND_CLICK);
 	} break;
 
 	default:
 		break;
 	}
+}
+
+static void drawRanking(void)
+{
+    view_render.setTextSize(1);
+    view_render.setTextColor(WHITE);
+
+    /* Ranking title */
+    view_render.setCursor(45, 5);
+    view_render.print("RANKING");
+
+    /* Draw ranking list */
+    for (uint8_t i = 0; i < RANKING_MAX; i++)
+    {
+        uint8_t y = 18 + (i * 9);
+
+        view_render.setCursor(10, y);
+        view_render.print(i + 1);
+
+        view_render.setCursor(25, y);
+        view_render.print(g_ranking[i].name);
+
+        view_render.setCursor(80, y);
+        view_render.print(g_ranking[i].score);
+    }
 }
